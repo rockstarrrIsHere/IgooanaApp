b813eaa21f9e0023ef044d88402b8ece8a7d1c51
Valentin Vasilyev <valentin.vasilyev@outlook.com>
2014-02-03 09:11:53 +0400

    Implement basic authentication: For authentication to work a Resources/Env.resx resource file should be copied, it's currently ignored in Git. This is done to prevent showing google analytics keys in Github.

---
+++ IgooanaApp/Auth.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Phone.Controls;
+using System.Windows;
+using Igooana;
+using IgooanaApp.Resources;
+
+namespace IgooanaApp {
+  public partial class Auth : PhoneApplicationPage {
+    private Api api;
+    public Auth() {
+      InitializeComponent();
+    }
+    private void AuthLoaded(object sender, RoutedEventArgs e) {
+      api = new Api(Env.ApiClientId, Env.ApiClientSecret);
+      Browser.Navigate(api.AuthenticateUri);
+    }
+
+    private async void OnNavigating(object sender, NavigatingEventArgs e) {
+      if (await api.Authenticate(e.Uri)) {
+        MessageBox.Show("You've authenticated with Google");
+      }
+    }
+  }
+}
