4ac41dab54ad5c0459ae239a2e884db75a952d3d
Valentin Vasilyev <valentin.vasilyev@outlook.com>
2014-02-10 11:24:10 +0400

    1st version of side menu.

---
+++ IgooanaApp/Layout.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace IgooanaApp {
+  public class Layout : ContentControl {
+    private bool menuOpen = false;
+    public Layout() {
+      DefaultStyleKey = typeof(Layout);
+    }
+
+    public override void OnApplyTemplate() {
+      base.OnApplyTemplate();
+      Button openMenuButton = GetTemplateChild("OpenMenuButton") as Button;
+      if (openMenuButton != null) {
+        openMenuButton.Click += (s, e) => {
+          if (menuOpen) {
+            VisualStateManager.GoToState(this, "MenuClosedState", true);
+            menuOpen = false;
+          }
+          else {
+            VisualStateManager.GoToState(this, "MenuOpenState", true);
+            menuOpen = true;
+          }
+        };
+      }
+    }
+  }
+}
