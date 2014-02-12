c04a29c6110c4dda1a8882af97405b4dc2578220
Valentin Vasilyev <valentin.vasilyev@outlook.com>
2014-02-12 11:59:52 +0400

    Add progress bar to acquisition dashboard control. Also researched a bit the databinding to dynamic objects. For now we need to map dynamics to anonymous objects.

--- IgooanaApp/Controls/DashboardAcquisition.xaml.cs
+++ IgooanaApp/Controls/DashboardAcquisition.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Igooana;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,11 +11,24 @@ namespace IgooanaApp.Controls {
       Loaded += OnLoaded;
     }
 
+    private void ToggleLoading() {
+      ProgressBar.Visibility = ToggleVisibility(ProgressBar.Visibility);
+      AcquisitionList.Visibility = ToggleVisibility(AcquisitionList.Visibility);
+    }
+
+    private Visibility ToggleVisibility(Visibility visibility) {
+      if (visibility == System.Windows.Visibility.Collapsed) {
+        return System.Windows.Visibility.Visible;
+      }
+      return System.Windows.Visibility.Collapsed;
+    }
+
     async void OnLoaded(object sender, RoutedEventArgs e) {
       var query = Query.For(AppState.Current.Profile.Id, AppState.Current.StartDate, AppState.Current.EndDate)
         .WithMetrics(Metric.Visits).WithDimensions(Dimension.TrafficSources.Source);
       var result = await Api.Current.Execute(query);
-      AcquisitionList.ItemsSource = result.Values.Select(x=>(dynamic)x).Select(x => new { Source = x.Source, Visits = x.Visits });
+      AcquisitionList.ItemsSource = result.Values.Select(x => new { Source = x.Source, Visits = x.Visits });
+      ToggleLoading();
     }
   }
 }
