2306687a99c13fab9a74c66cb1268ee14549698c
Valentin Vasilyev <valentin.vasilyev@outlook.com>
2014-03-30 07:29:53 -0700

    Add online connectivity timeout of 10 seconds

--- IgooanaApp/NetworkConnectivity.cs
+++ IgooanaApp/NetworkConnectivity.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Windows.Networking;
 using Windows.Networking.Sockets;
 
 namespace IgooanaApp {
   public static class NetworkConnectivity {
+    const int TimeoutMilliseconds = 10000;
     public static async Task<bool> IsOnline() {
       var googleHost = new HostName("google.com");
       var socket = new StreamSocket();
       try {
-        await socket.ConnectAsync(googleHost, "http");
+        var cts = new CancellationTokenSource(TimeoutMilliseconds);
+        await socket.ConnectAsync(googleHost, "http").AsTask(cts.Token);
         return socket.Information.RemoteAddress != null;
       } catch {
         return false;
