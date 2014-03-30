ab615d3dccbaaede9f10f43c67d64404679e4915
Valentin Vasilyev <valentin.vasilyev@outlook.com>
2014-03-30 01:36:16 -0700

    Resolution independence
    Added classes and resources to use image assets in a resolution
    independent way.

---
+++ IgooanaApp/MultiResImage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Igooana.Extensions;
+
+namespace IgooanaApp {
+  public enum ImageFormat { JPG, PNG }
+  public static class MultiResImage {
+    /// <summary>
+    /// Gets the full path to the image asset, suitable for current screen resolution
+    /// </summary>
+    /// <param name="path">Path to image asset, relative to app root</param>
+    /// <returns></returns>
+    public static Uri GetUri(string path, ImageFormat imageFormat = ImageFormat.JPG) {
+      switch (ResolutionHelper.CurrentResolution) {
+        case Resolutions.HD:
+          return new Uri("{0}.Screen-720p.{1}".FormatWith(path, imageFormat.ToString().ToLowerInvariant()), UriKind.Relative);
+        case Resolutions.WXGA:
+          return new Uri("{0}.Screen-WXGA.{1}".FormatWith(path, imageFormat.ToString().ToLowerInvariant()), UriKind.Relative);
+        case Resolutions.WVGA:
+          return new Uri("{0}.Screen-WVGA.{1}".FormatWith(path, imageFormat.ToString().ToLowerInvariant()), UriKind.Relative);
+        default:
+          throw new InvalidOperationException("Unknown resolution type");
+      }
+    }
+  }
+}
---
+++ IgooanaApp/MultiResSplashScreenImage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace IgooanaApp {
+  public class MultiResSplashScreenImage {
+    public Uri Uri {
+      get {
+        return MultiResImage.GetUri("SplashScreenImage");
+      }
+    }
+  }
+}
