96ac4f2df3cecb5a69d4e87cb61a12f225e77601
NNastasya <n_nastasya@mail.ru>
2014-04-03 22:30:16 +0400

    adds control NoConnection

---
+++ IgooanaApp/Controls/NoConnection.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace IgooanaApp.WP8.Controls {
+  public partial class NoConnection : UserControl {
+    public NoConnection() {
+      InitializeComponent();
+    }
+  }
+}
