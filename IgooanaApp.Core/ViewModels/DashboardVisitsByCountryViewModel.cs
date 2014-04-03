ad4950a64adac216c99eb3049df715252745a978
Valentin Vasilyev <valentin.vasilyev@outlook.com>
2014-04-03 12:28:47 +0400

    Continue refactoring

--- IgooanaApp.Core/ViewModels/DashboardVisitsByCountryViewModel.cs
+++ IgooanaApp.Core/ViewModels/DashboardVisitsByCountryViewModel.cs
@@ -1,62 +1,57 @@
 ﻿using IgooanaApp.Core;
+using IgooanaApp.Core.Resources;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 
 namespace IgooanaApp.ViewModels {
   public class DashboardVisitsByCountryViewModel {
-    ///// <summary>
-    ///// Show n top data rows 
-    ///// </summary>
-    //const int SHOW_ROWS_COUNT = 10;
-    //private int totalVisitsCount;
-    //private int restCountriesCount;
-    //private IEnumerable<dynamic> visitsByCountry= new List<string>();
+    /// <summary>
+    /// Show n top data rows 
+    /// </summary>
+    const int ShowRowsCount = 10;
+    const int CharPxLength = 16;
+    private int totalVisitsCount;
+    private int restCountriesCount;
+    private IEnumerable<dynamic> visitsByCountry = new List<string>();
 
-    //public int VisitsTotal {
-    //  get {
-    //    return totalVisitsCount;
-    //  }
-    //}
-    //public IEnumerable<dynamic> VisitsByCountry { get { return visitsByCountry; } }
-    //public string CountriesOverall { 
-    //  get {
-    //    return restCountriesCount > 1 ? String.Format(LocalizedStrings.PluralizedTemplate("VisitsByCountryCountriesRestTemplate", restCountriesCount), restCountriesCount)
-    //      : String.Empty;
-    //    } 
-    //}
+    public int VisitsTotal {
+      get {
+        return totalVisitsCount;
+      }
+    }
+    public IEnumerable<dynamic> VisitsByCountry { get { return visitsByCountry; } }
+    public string CountriesOverall {
+      get {
+        return restCountriesCount > 1 ? String.Format(LocalizedStrings.PluralizedTemplate("VisitsByCountryCountriesRestTemplate", restCountriesCount), restCountriesCount)
+          : String.Empty;
+      }
+    }
 
-    //public bool HasData { get { return visitsByCountry.Count() > 0;} }
+    public bool HasData { get { return visitsByCountry.Any(); } }
 
     //public Thickness PaddingCountriesOverall { get; private set; }
 
-    //public DashboardVisitsByCountryViewModel(IEnumerable<dynamic> gaRows, dynamic total, dynamic overallViewSettings) {
-    //  try {
-    //    var listOfVisits = gaRows.Take(SHOW_ROWS_COUNT).Select(x => new {Visitors = x.Visitors, Country = x.Country});
-    //    totalVisitsCount = total.Visitors;
-    //    restCountriesCount = gaRows.Count() - SHOW_ROWS_COUNT;
+    public DashboardVisitsByCountryViewModel(IEnumerable<dynamic> gaRows, dynamic total) {
+      try {
+        restCountriesCount = gaRows.Count() - ShowRowsCount;
+        var listOfVisits = gaRows.Take(ShowRowsCount).Select(x => new { Visitors = x.Visitors, Country = x.Country });
+        totalVisitsCount = total.Visitors;
 
-    //    int maxDigitsCount = (gaRows.First().Visitors).ToString("N0").Length;
-    //    visitsByCountry = listOfVisits.Select((item, index) => new { 
-    //      Count = item.Visitors.ToString("N0"), 
-    //      Text = String.Format(Localization.VisitsByCountryStringTemplate, item.Country),
-    //      Number = index + 1, 
-    //      CountFieldWidth = (maxDigitsCount) * overallViewSettings.LetterWidth });
+        int maxDigitsCount = (gaRows.First().Visitors).ToString("N0").Length;
+        visitsByCountry = listOfVisits.Select((item, index) => new {
+          Count = item.Visitors.ToString("N0"),
+          Text = String.Format(L.VisitsByCountryStringTemplate, item.Country),
+          Number = index + 1,
+          CountFieldWidth = (maxDigitsCount) * CharPxLength
+        });
 
-    //    //OtherCountriesItemMargin should be Bullet width + Count margin
-    //    PaddingCountriesOverall = new Thickness(overallViewSettings.LetterWidth * (maxDigitsCount > overallViewSettings.Level ? maxDigitsCount - overallViewSettings.Level : 0) , 0, 0, 0);
-    //  }
-    //  catch (Exception ex) {
-    //    //TODO ex handling
-    //    throw ex;
-    //  }
-
-    //}
-
-
-
+        //OtherCountriesItemMargin should be Bullet width + Count margin
+        //PaddingCountriesOverall = new Thickness(overallViewSettings.LetterWidth * (maxDigitsCount > overallViewSettings.Level ? maxDigitsCount - overallViewSettings.Level : 0), 0, 0, 0);
+      } catch (Exception ex) {
+        //TODO ex handling
+        throw ex;
+      }
+    }
   }
 }
--- IgooanaApp/Controls/DashboardVisitsByCountry.xaml.cs
+++ IgooanaApp/Controls/DashboardVisitsByCountry.xaml.cs
@@ -1,50 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using Igooana;
+using IgooanaApp.Core;
+using IgooanaApp.ViewModels;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Navigation;
-using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
-using Igooana;
 
 namespace IgooanaApp.WP8.Controls {
   public partial class DashboardVisitsByCountry : UserControl {
-
-    //const int visitsCountLetterWidth = 16;
-    //const int startCountOverallTextLevel = 2;
+    const int visitsCountLetterWidth = 16;
+    const int startCountOverallTextLevel = 2;
 
     public DashboardVisitsByCountry() {
       InitializeComponent();
-      //Loaded += DashboardVisitsByCountry_Loaded;
+      Loaded += DashboardVisitsByCountry_Loaded;
     }
-    //private void ToggleLoading(bool hasData) {
-    //  ProgressBar.Visibility = ViewHelper.ToggleVisibility(ProgressBar.Visibility);
-    //  VisitsLayout.Visibility = ViewHelper.ToggleVisibility(VisitsLayout.Visibility);
-    //  if (hasData) {
-    //    NoDataText.Visibility = ViewHelper.ToggleVisibility(NoDataText.Visibility);
-    //    CountriesListPanel.Visibility = ViewHelper.ToggleVisibility(CountriesListPanel.Visibility);
-    //  }
-    //}
+    private void ToggleLoading(bool hasData) {
+      ProgressBar.Visibility = ViewHelper.ToggleVisibility(ProgressBar.Visibility);
+      VisitsLayout.Visibility = ViewHelper.ToggleVisibility(VisitsLayout.Visibility);
+      if (hasData) {
+        NoDataText.Visibility = ViewHelper.ToggleVisibility(NoDataText.Visibility);
+        CountriesListPanel.Visibility = ViewHelper.ToggleVisibility(CountriesListPanel.Visibility);
+      }
+    }
 
-    //private void ToggleIfNoData() {
-     
-    //}
+    private void ToggleIfNoData() {
 
-    //async void DashboardVisitsByCountry_Loaded(object sender, RoutedEventArgs e) {
-    //  Query query = Query.For(AppState.Current.Profile.Id, AppState.Current.StartDate, AppState.Current.EndDate)
-    //    .WithDimensions(Dimension.GeoNetwork.Country)
-    //    .WithMetrics(Metric.Visitor.Visitors)
-    //    //.Take(SHOW_ROWS_COUNT)
-    //    .OrderByDescending(Metric.Visitor.Visitors);
-      
-    //  var result = await Api.Current.Execute(query);
-    //  var viewModel = new DashboardVisitsByCountryViewModel(result.Values, result.Totals, new { LetterWidth = visitsCountLetterWidth, Level = startCountOverallTextLevel });
-     
-    //  DataContext = viewModel;
-      
-    //  ToggleLoading(viewModel.HasData);
-    //}
+    }
+
+    async void DashboardVisitsByCountry_Loaded(object sender, RoutedEventArgs e) {
+      Query query = Query.For(AppState.Current.Profile.Id, AppState.Current.StartDate, AppState.Current.EndDate)
+        .WithDimensions(Dimension.GeoNetwork.Country)
+        .WithMetrics(Metric.Visitor.Visitors)
+        //.Take(SHOW_ROWS_COUNT)
+        .OrderByDescending(Metric.Visitor.Visitors);
+
+      var result = await Api.Current.Execute(query);
+      var viewModel = new DashboardVisitsByCountryViewModel(result.Values, result.Totals);
+
+      DataContext = viewModel;
+
+      ToggleLoading(viewModel.HasData);
+    }
   }
 }
